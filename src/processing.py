8fbbb7fc2f729c490e6c3ab9445ea2a89b5185af
Anna Kordonova <anakordon4@yandex.ru>
2024-08-21 18:05:56 +0300

    latest version homework_10_1

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,17 @@
+from operator import itemgetter
 from typing import Any
 
 
-def filter_by_state(data: list[dict[str, Any]], state = "EXECUTED") -> list[dict[str, Any]]:
+def filter_by_state(data: list[dict[str, Any]], state="EXECUTED") -> list[dict[str, Any]]:
+    """Функция, возвращающая список словарей по определённому ключу state"""
     new_list = []
     for operation in data:
-        if operation.get("state") == state:
+        if operation["state"] == state:
             new_list.append(operation)
     return new_list
 
 
 def sort_by_date(data: list[dict[str, Any]], ascending=True) -> list[dict[str, Any]]:
-    result = sorted(data, key=lambda x: x.get("date"), reverse=ascending)
+    """Функция, возвращающая список словарей отсортированных по дате"""
+    result = sorted(data, key=itemgetter("date"))
     return result
-
