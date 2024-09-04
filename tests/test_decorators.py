d8124b3a4ac13324e062d57e2af26ccbc1b702d1
Anna Kordonova <anakordon4@yandex.ru>
2024-09-04 19:53:04 +0300

    minor changes

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -19,7 +19,7 @@ def fail_func(x: int | float, y: int | float) -> int | float:
 def test_success_func(capsys: Any) -> Any:
     success_func(2, 3)
     captured = capsys.readouterr()
-    assert captured.out == "Начало работы функции 'success_func'\nКонец работы функции 'success_func'\n"
+    assert captured.out == f"Начало работы функции 'success_func'\n5Конец работы функции 'success_func'\n"
 
 
 def test_fail_func() -> None:
@@ -29,4 +29,4 @@ def test_fail_func() -> None:
     assert os.path.exists("mylog.txt")
     with open("mylog.txt", "r", encoding="utf-8") as file:
         test_log_text = file.read()
-        assert test_log_text == "'fail_func' error: ZeroDivisionError. Inputs: (1, 0), {}\n"
+        assert test_log_text == "Начало работы функции 'fail_func'\n'fail_func' error: ZeroDivisionError. Inputs: (1, 0), {}\n"
