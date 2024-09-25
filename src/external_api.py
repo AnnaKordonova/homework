f0b067ec0cad8c45da9c3b92c130f8f2a8bb8248
Anna Kordonova <anakordon4@yandex.ru>
2024-09-25 14:32:01 +0300

    update external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -8,13 +8,15 @@ load_dotenv()
 values = os.getenv("API_KEY")
 
 
-def convert_to_rub(transaction: Any) -> Any:
+def convert_to_rub(transaction: Any) -> float:
     """Функция конвертации"""
-    amout = transaction["operationAmount"]["amount"]
+    amount = transaction["operationAmount"]["amount"]
     code = transaction["operationAmount"]["currency"]["code"]
+    if code == "RUB":
+        return float(amount)
     to = "RUB"
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amout}"
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amount}"
     payload = {}
     response = requests.get(url, headers={"apikey": values}, data=payload)
     result = response.json()
-    return result["result"]
+    return float(result["result"])
